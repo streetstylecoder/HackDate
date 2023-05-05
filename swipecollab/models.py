05939dd9bb1209a1321a6212ad505cfdcb52ba4e
Somya Sethi <105233506+somyasethii@users.noreply.github.com>
2023-05-05 11:18:25 +0530

    modified models

--- swipecollab/models.py
+++ swipecollab/models.py
@@ -2,14 +2,3 @@ from django.db import models
 
 # Create your models here.
 
-class swipeprofile(models.Model):
-    name=models.CharField(max_length=100)
-    Looking_for=models.CharField(max_length=100)
-    Description=models.TextField()
-    analyse_profile=models.CharField(max_length=100)
-
-
-class savematches(models.Model):
-    name=models.CharField(max_length=100,default="match name")
-    userid_sender=models.BinaryField()
-    userid_rec=models.BinaryField()
